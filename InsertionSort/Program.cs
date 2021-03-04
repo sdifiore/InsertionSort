54a81d1f7dca8358294f89e1abd117eef18af7d8
Sergio Di Fiore <difiores@outlook.com>
2021-03-04 15:04:20 -0300

    Rearranjo dos arquivos e funções

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -6,37 +6,12 @@ namespace InsertionSort
     {
         static void Main()
         {
-            Console.WriteLine("Quantos dados deseja classificar?");
-            Console.WriteLine("ENTER para 100");
-            var ehNumero = int.TryParse(Console.ReadLine(), out int dimensao);
-
-            if (!ehNumero)
-                dimensao = 100;
-
-            string opcao = "0";
-
-            while (opcao != "1" && opcao != "2")
-            {
-                Console.WriteLine();
-                Console.WriteLine("Escolha a opção de classificação:");
-                Console.WriteLine("[1] Ascendente    [2] Descendente");
-                opcao = Console.ReadKey().KeyChar.ToString();
-            }
+            var dimensao = ProgramBase.GetDimension();
+            var opcao = ProgramBase.GetSentido();
 
             Console.Clear();
 
-            double[] sortArray = new double[dimensao];
-
-            for (int i = 0; i < dimensao; i++)
-            {
-                ehNumero = false;
-
-                while (!ehNumero)
-                {
-                    Console.WriteLine($"Digite o valor do {i + 1}º elemento:");
-                    ehNumero = double.TryParse(Console.ReadLine(), out sortArray[i]);
-                }
-            }
+            var sortArray = ProgramBase.GetArray(dimensao);
 
             Console.Clear();
             Console.WriteLine("** Início do processamento - Aguarde **");
@@ -48,15 +23,7 @@ namespace InsertionSort
             else
                 resultado = Classifica.Descende(dimensao, sortArray);
 
-            PrintArray(resultado);
-        }
-        private static void PrintArray(ArrayOrdenadocomTempo array)
-        {
-            foreach (var item in array.arrayValues)
-                Console.WriteLine(item);
-
-            Console.WriteLine();
-            Console.WriteLine($"Tempo de processamento: {array.Duracao}");
+            ProgramBase.PrintArray(resultado);
         }
     }
 }
---
+++ InsertionSort/ProgramBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    public static class ProgramBase
+    {
+        public static void PrintArray(ArrayOrdenadocomTempo array)
+        {
+            foreach (var item in array.arrayValues)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+            Console.WriteLine($"Tempo de processamento: {array.Duracao}");
+        }
+
+        public static int GetDimension()
+        {
+            Console.WriteLine("Quantos dados deseja classificar?");
+            Console.WriteLine("ENTER para 100");
+            var ehNumero = int.TryParse(Console.ReadLine(), out int dimensao);
+
+            if (!ehNumero)
+                dimensao = 100;
+
+            return dimensao;
+        }
+
+        public static string GetSentido()
+        {
+            var opcao = "0";
+
+            while (opcao != "1" && opcao != "2")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Escolha a opção de classificação:");
+                Console.WriteLine("[1] Ascendente    [2] Descendente");
+                opcao = Console.ReadKey().KeyChar.ToString();
+            }
+
+            return opcao;
+        }
+
+        public static double[] GetArray(int dimensao)
+        {
+            double[] sortArray = new double[dimensao];
+
+            for (int i = 0; i < dimensao; i++)
+            {
+                var ehNumero = false;
+
+                while (!ehNumero)
+                {
+                    Console.WriteLine($"Digite o valor do {i + 1}º elemento:");
+                    ehNumero = double.TryParse(Console.ReadLine(), out sortArray[i]);
+                }
+            }
+
+            return sortArray;
+        }
+    }
+}
